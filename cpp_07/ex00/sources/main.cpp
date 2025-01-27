2acd6900e658cdbcaa92bb505d25aedda2ddc93c
Yaroslav <wikifixcom@gmail.com>
2025-01-27 18:46:01 +0000

    remove .DS_Store and update cpp07/ex00 'main.cpp'

--- cpp_07/ex00/sources/main.cpp
+++ cpp_07/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yadereve <yadereve@student.42lisboa.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 10:53:55 by yadereve          #+#    #+#             */
-/*   Updated: 2025/01/26 11:37:12 by yadereve         ###   ########.fr       */
+/*   Updated: 2025/01/26 12:38:48 by yadereve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,19 @@ int main()
 	std::cout << "After swap:\n" << "s1 = " << s1 << ", s2 = " << s2 << std::endl;
 	std::cout << "max(s1, s2): " << ::max(s1, s2) << std::endl;
 	std::cout << "min(s1, s2): " << ::min(s1, s2) << std::endl;
-	
+
+	//int a = 2;
+	//int b = 3;
+	//::swap( a, b );
+	//std::cout << "a = " << a << ", b = " << b << std::endl;
+	//std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	//std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	//std::string c = "chaine1";
+	//std::string d = "chaine2";
+	//::swap(c, d);
+	//std::cout << "c = " << c << ", d = " << d << std::endl;
+	//std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	//std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
 	return 0;
 }
