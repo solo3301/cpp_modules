7a2829d837159992170c2fcb805f017b66f35e3a
Yaroslav <wikifixcom@gmail.com>
2025-01-27 18:42:49 +0000

    add cpp07/ex01

---
+++ cpp_07/ex01/includes/iter.hpp
@@ -0,0 +1,32 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <cstddef>
+#include <iostream>
+
+template <typename T>
+void iter(T* array, size_t len, void (*func)(T&))
+{
+	for (size_t i = 0; i < len; i++)
+		func(array[i]);
+}
+
+template <typename T>
+void print(T& value)
+{
+	std::cout << value << " ";
+}
+
+template <typename T>
+void increment(T& value)
+{
+	value++;
+}
+
+void makeUppercase(char& c)
+{
+	if (std::islower(c))
+		c = std::toupper(c);
+}
+
+#endif
---
+++ cpp_07/ex01/sources/main.cpp
@@ -0,0 +1,37 @@
+#include "../includes/iter.hpp"
+
+int main()
+{
+	int numbers[] = {1, 2, 3, 4, 5};
+	size_t numLen = sizeof(numbers) / sizeof(numbers[0]);
+	float floats[] = {.1, .2, .3, .4, .5};
+	size_t floatLen = sizeof(numbers) / sizeof(numbers[0]);
+	char chars[] = {'a', 'b', 'c'};
+	size_t charLen = sizeof(chars) / sizeof(chars[0]);
+
+	std::cout << "Origin numbers: " << std::endl;
+	iter(numbers, numLen, print);
+	std::cout << std::endl;
+	iter(numbers, numLen, increment);
+	std::cout << "Incremented numbers: " << std::endl;
+	iter(numbers, numLen, print);
+	std::cout << std::endl;
+
+	std::cout << "\nOrigin floats: " << std::endl;
+	iter(floats, floatLen, print);
+	std::cout << std::endl;
+	iter(floats, floatLen, increment);
+	std::cout << "Incremented floats: " << std::endl;
+	iter(floats, floatLen, print);
+	std::cout << std::endl;
+
+	std::cout << "\nOriginal chars: " << std::endl;
+	iter(chars, charLen, print);
+	std::cout << std::endl;
+	iter(chars, charLen, makeUppercase);
+	std::cout << "Uppercase chars: " << std::endl;
+	iter(chars, charLen, print);
+	std::cout << std::endl;
+
+			return 0;
+}
